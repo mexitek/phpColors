8b8cdaa72b70e6ac506e2abf2d3edf094706419b
Arlo Carreon <arlo.carreon@gmail.com>
2012-09-04 17:09:32 -0400

    Adding initial state of project

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-phpColors
-=========
+# phpColors
 
 A series of PHP classes that let you manipulate colors.
+
+## Current
+- HEX-To-HSL : Not working
+- HSL-To-HEX : Working
+
+## Goal
+- Darken.php : Takes a HEX and darkens X percentage. Will have to convert to HSL first, then decrease the L value.
+- Lighten.php : Takes a HEX and lightens X percentage. Will have to convert to HSL first, then increase the L value.
---
+++ hextohsl.php
@@ -0,0 +1,66 @@
+<?php
+class HexToHsl {
+    
+    /**
+     * Outputs a given color
+     *
+     * @param string $color The color to convert to HSV array
+     * @return string
+     */
+    public function render($hexColor = FALSE) {
+        // Strip # sign is present
+        $color = str_replace("#", "", $hexColor);
+        
+        // Make sure it's 6 digits
+        if( strlen($color) == 3 ) {
+            $color = $color[0].$color[0].$color[1].$color[1].$color[2].$color[2];
+        } else if( strlen($color) != 6 ) {
+            throw new Exception("HEX color needs to be 6 or 3 digits long");
+        }
+        
+        // Convert HEX to DEC
+        $R = hexdec($color[0].$color[1]);
+        $G = hexdec($color[2].$color[3]);
+        $B = hexdec($color[4].$color[5]);
+        
+        $HSL = array();
+
+        $var_R = ($R / 255);
+        $var_G = ($G / 255);
+        $var_B = ($B / 255);
+
+        $var_Min = min($var_R, $var_G, $var_B);
+        $var_Max = max($var_R, $var_G, $var_B);
+        $del_Max = $var_Max - $var_Min;
+
+        $L = $var_Max;
+
+        if ($del_Max == 0)
+        {
+            $H = 0;
+            $S = 0;
+        }
+        else
+        {
+            $S = $del_Max / $var_Max;
+
+            $del_R = ( ( ( $var_Max - $var_R ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
+            $del_G = ( ( ( $var_Max - $var_G ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
+            $del_B = ( ( ( $var_Max - $var_B ) / 6 ) + ( $del_Max / 2 ) ) / $del_Max;
+
+            if      ($var_R == $var_Max) $H = $del_B - $del_G;
+            else if ($var_G == $var_Max) $H = ( 1 / 3 ) + $del_R - $del_B;
+            else if ($var_B == $var_Max) $H = ( 2 / 3 ) + $del_G - $del_R;
+
+            if ($H<0) $H++;
+            if ($H>1) $H--;
+        }
+
+        $HSL['H'] = $H;
+        $HSL['S'] = $S;
+        $HSL['L'] = $L;
+
+        return $HSL;
+    }
+
+}

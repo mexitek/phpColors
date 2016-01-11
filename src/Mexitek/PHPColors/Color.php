71ca9d816634a3f8d28f8b3a41af8c5f6a20e007
João Pina <joao-t-pina@telecom.pt>
2016-01-11 15:31:06 +0000

    isLight and isDark with lighter or darker coefficient variable

--- src/Mexitek/PHPColors/Color.php
+++ src/Mexitek/PHPColors/Color.php
@@ -263,9 +263,10 @@ class Color {
     /**
      * Returns whether or not given color is considered "light"
      * @param string|Boolean $color
+     * @param int $lighterThan
      * @return boolean
      */
-    public function isLight( $color = FALSE ){
+    public function isLight( $color = FALSE, $lighterThan = 130 ){
         // Get our color
         $color = ($color) ? $color : $this->_hex;
 
@@ -274,15 +275,16 @@ class Color {
         $g = hexdec($color[2].$color[3]);
         $b = hexdec($color[4].$color[5]);
 
-        return (( $r*299 + $g*587 + $b*114 )/1000 > 130);
+        return (( $r*299 + $g*587 + $b*114 )/1000 > $lighterThan);
     }
 
     /**
      * Returns whether or not a given color is considered "dark"
      * @param string|Boolean $color
+     * @param int $darkerThen
      * @return boolean
      */
-    public function isDark( $color = FALSE ){
+    public function isDark( $color = FALSE, $darkerThen = 130 ){
         // Get our color
         $color = ($color) ? $color:$this->_hex;
 
@@ -291,7 +293,7 @@ class Color {
         $g = hexdec($color[2].$color[3]);
         $b = hexdec($color[4].$color[5]);
 
-        return (( $r*299 + $g*587 + $b*114 )/1000 <= 130);
+        return (( $r*299 + $g*587 + $b*114 )/1000 <= $darkerThen);
     }
 
     /**
